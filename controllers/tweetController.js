640b08fe735a93fe579d7aa39a8f7c952293acf3
Sebasan22 <callofsebas@hotmail.com>
2023-03-13 19:53:34 -0300

    destroy tweet

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -32,10 +32,20 @@ async function update(req, res) {
   });
   res.json(tweet);
 }
+async function destroy(req, res) {
+  const user = await User.findById("63fc0a422b6e9606a49c2f47");
+  const tweet = req.params.id;
+  await Tweet.findByIdAndDelete(tweet);
+  await User.findByIdAndUpdate(user, {
+    $pull: { tweets: tweet },
+  });
+  res.json(user);
+}
 
 module.exports = {
   index,
   show,
   store,
   update,
+  destroy,
 };
--- routes/tweetRoutes.js
+++ routes/tweetRoutes.js
@@ -6,5 +6,6 @@ router.get("/", tweetController.index);
 router.get("/:id", tweetController.show);
 router.post("/", tweetController.store);
 router.patch("/:id", tweetController.update);
+router.delete("/:id", tweetController.destroy);
 
 module.exports = router;
