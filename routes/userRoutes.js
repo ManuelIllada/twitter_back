763c6a9462eeaa22145f5f0ddd7c41803738288f
Juan Nanio <naniojuan@gmail.com>
2023-03-14 10:36:47 -0300

    cors

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const { expressjwt: checkJwt } = require("express-jwt");
+const cors = require("cors");
 
 const userController = require("../controllers/userController");
+router.post("/", userController.token);
+
+router.use(cors);
 
 router.get("/", userController.index);
 router.get("/:id", userController.show);
 router.post("/", userController.store);
 router.patch("/:id", userController.update);
 router.delete("/:id", userController.destroy);
-router.post("/", userController.token);
 
 module.exports = router;
 
+// Middleware
 checkJwt({ secret: "Un string secreto", algorithms: ["HS256"] });
