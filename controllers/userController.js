df61c898135071e78c84b67ceb39f7196eb66a99
Juan Nanio <naniojuan@gmail.com>
2023-03-13 21:54:22 -0300

    user!

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 const User = require("../models/User");
+const formidable = require("formidable");
+const bcrypt = require("bcryptjs");
+const Tweet = require("../models/Tweet");
 
 async function index(req, res) {
   const users = await User.find();
@@ -9,7 +12,45 @@ async function show(req, res) {
   res.json(user);
 }
 
+async function store(req, res) {
+  const form = formidable({
+    multiples: true,
+    uploadDir: __dirname + "/../public/img",
+    keepExtensions: true,
+  });
+
+  form.parse(req, async (err, fields, files) => {
+    const passwordParaHashear = fields.password;
+    const passwordHasheado = await bcrypt.hash(passwordParaHashear, 10);
+    const { firstname, lastname, email, username } = fields;
+    await User.create({
+      firstname,
+      lastname,
+      email,
+      username,
+      password: passwordHasheado,
+      image: files.image.newFilename,
+    });
+  });
+  res.json("json de nuevo user");
+}
+async function update(req, res) {
+  const user = await User.findByIdAndUpdate(req.params.id, {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+  });
+  res.json(user);
+}
+async function destroy(req, res) {
+  const user = await User.findById(req.params.id);
+  await User.findByIdAndDelete(user);
+  await Tweet.deleteMany({ userId: req.params.id });
+  res.json(user);
+}
 module.exports = {
   index,
   show,
+  store,
+  update,
+  destroy,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,8 @@ const userController = require("../controllers/userController");
 
 router.get("/", userController.index);
 router.get("/:id", userController.show);
+router.post("/", userController.store);
+router.patch("/:id", userController.update);
+router.delete("/:id", userController.destroy);
 
 module.exports = router;
